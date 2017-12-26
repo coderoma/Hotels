2ded5136ea88b480e2ba52cbe02dcbd05e76b9c0
Roman <r.reset@gmail.com>
2017-12-26 16:50:27 +0700

    savepoint

--- js/hotels-list.js
+++ js/hotels-list.js
@@ -1,8 +1,7 @@
+'use strict';
+
 var container = document.querySelector( '.hotels-list' );
-
-
 var loadedData = null;
-
 var xhr = new XMLHttpRequest();
 
 /**
@@ -10,13 +9,18 @@ var xhr = new XMLHttpRequest();
  * @param {string} URL
  * @param {boolean} async
  */
-xhr.open('GET', 'hotels.json', false)
+xhr.open( 'GET', '../data/hotels.json' )
+xhr.timeout = 10000;
+xhr.onload = function( evt ) {
+  console.log(JSON.parse(evt.srcElement.response));
+};
+xhr.send();
 
 
-hotels.forEach( function( hotel ) {
-	var element = getElementFromTemplate( hotel );
-	container.appendChild( element );
-} );
+// hotels.forEach( function( hotel ) {
+//   var element = getElementFromTemplate( hotel );
+//   container.appendChild( element );
+// } );
 
 
 /**
@@ -27,38 +31,38 @@ function getElementFromTemplate( data ) {
   var template = document.querySelector( '#hotel-template' );
   var IMAGE_TIMEOUT = 10000;
 
-	if ( 'content' in template ) {
-		var element = template.content.children[ 0 ].cloneNode( true );
-	} else {
-    var element = template.children[0].cloneNode( true );
+  if ( 'content' in template ) {
+    var element = template.content.children[ 0 ].cloneNode( true );
+  } else {
+    var element = template.children[ 0 ].cloneNode( true );
   }
-  
+
   var rating = element.querySelector( '.hotel__rating' );
   element.querySelector( '.hotel__name' ).textContent = data.name;
   element.querySelector( '.hotel__price-value' ).textContent = data.price;
 
-  data.rating ? rating.textContent = data.rating : rating.style.display = "none";  
-  
+  data.rating ? rating.textContent = data.rating : rating.style.display = "none";
+
   // prepare image
   var backgroundImage = new Image(); //create image
 
-  var imageLoadTimeout = setTimeout(function(){ //fallback if image is cannot load
+  var imageLoadTimeout = setTimeout( function() { //fallback if image is cannot load
     backgroundImage.src = '';
-    element.classList.add('hotel--nophoto'); // give element class with default background-image
-  }, IMAGE_TIMEOUT);
+    element.classList.add( 'hotel--nophoto' ); // give element class with default background-image
+  }, IMAGE_TIMEOUT );
 
-// load and erorr handlers we set before changing .src attribute
+  // load and erorr handlers we set before changing .src attribute
 
-  backgroundImage.onload = function(){ 
-    clearTimeout(imageLoadTimeout);
+  backgroundImage.onload = function() {
+    clearTimeout( imageLoadTimeout );
     element.style.backgroundImage = 'url(\'' + backgroundImage.src + '\')';
-  }
-  backgroundImage.onerror = function(){
-    element.classList.add('hotel--nophoto');
-  }
+  };
+  backgroundImage.onerror = function() {
+    element.classList.add( 'hotel--nophoto' );
+  };
 
-// start all this
+  // start all this
   backgroundImage.src = data.preview ? data.preview : '';
 
-	return element;
+  return element;
 }
--- js/main.js
+++ js/main.js
@@ -13,36 +13,34 @@ guests.max = 6;
 var MAX_GUESTS_PER_ROOM = 3;
 
 function setMinAndMaxRooms( roomsElement, guestNumber ) {
-	roomsElement.min = Math.ceil( guestNumber / MAX_GUESTS_PER_ROOM );
-	roomsElement.max = guestNumber
+  roomsElement.min = Math.ceil( guestNumber / MAX_GUESTS_PER_ROOM );
+  roomsElement.max = guestNumber;
 }
 
-guests.value = docCookies.getItem('guests');
+guests.value = docCookies.getItem( 'guests' );
 setMinAndMaxRooms( rooms, guests.value );
-rooms.value = docCookies.getItem('rooms');
+rooms.value = docCookies.getItem( 'rooms' );
 
 
 guests.onchange = function() {
-	setMinAndMaxRooms( rooms, guests.value );
+  setMinAndMaxRooms( rooms, guests.value );
 };
 
 formElement.onsubmit = function( evt ) {
-	evt.preventDefault();
+  evt.preventDefault();
 
-	var dateToExpire = +Date.now() + 3 * 24 * 60 * 60 * 1000; // сегодня + вторая часть это через три дня
-	var formattedDateToExpire = new Date( dateToExpire ).toUTCString();
+  var dateToExpire = +Date.now() + 3 * 24 * 60 * 60 * 1000; // сегодня + вторая часть это через три дня
+  var formattedDateToExpire = new Date( dateToExpire ).toUTCString();
 
-	document.cookie = 'guests=' + guests.value + ';expires=' + formattedDateToExpire;
-	document.cookie = 'rooms=' + rooms.value + ';expires=' + formattedDateToExpire;
+  document.cookie = 'guests=' + guests.value + ';expires=' + formattedDateToExpire;
+  document.cookie = 'rooms=' + rooms.value + ';expires=' + formattedDateToExpire;
 
-	formElement.submit();
+  formElement.submit();
 }
 
 searchform.addEventListener( 'mouseover', function( event ) {
-	if ( event.target.classList.contains( 'searchform__guests' ) ) {
-		event.target.focus();
-	}
+  if ( event.target.classList.contains( 'searchform__guests' ) ) {
+    event.target.focus();
+  }
 
 } );
-
-
