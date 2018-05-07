1011113f7569f9ca5b50a3c55ef253599ecb3dd1
Roman <r.reset@gmail.com>
2018-05-07 20:41:39 +0700

    added gallery next button logic

--- js/gallery.js
+++ js/gallery.js
@@ -1,74 +1,84 @@
-(function(){
-  
+( function () {
+
   /**
    * @constructor
    */
-  var Gallery = function() {
-    this.element = document.querySelector('.gallery-overlay');
-    this._closeButton = this.element.querySelector('.gallery-overlay__close');
-    this._nextButton = this.element.querySelector('.gallery-overlay-control-right');
-    this._onCloseClick = this._onCloseClick.bind(this);
+  var Gallery = function () {
+    this.element = document.querySelector( '.gallery-overlay' );
+    this._closeButton = this.element.querySelector( '.gallery-overlay__close' );
+    this._nextButton = this.element.querySelector( '.gallery-overlay-control-right' );
+
+    this._onNextClick = this._onNextClick.bind( this );
+    this._onCloseClick = this._onCloseClick.bind( this );
   };
 
-  
+
   Gallery.prototype.show = function () {
-    this.element.classList.remove('hidden');
+    this.element.classList.remove( 'hidden' );
 
-    var thumbnailsContainer = this.element.querySelector('.gallery-overlay__thumbnails');
+    var thumbnailsContainer = this.element.querySelector( '.gallery-overlay__thumbnails' );
 
-    this.data.pictures.forEach(function(pic, i) {
-      var picture = new Image(); 
+    this.data.pictures.forEach( function ( pic, i ) {
+      var picture = new Image();
       picture.height = 40;
       picture.src = pic;
-      thumbnailsContainer.appendChild(picture); 
-    }, this);
+      thumbnailsContainer.appendChild( picture );
+    }, this );
 
-    this.setCurrentImage(0);
+    this.setCurrentImage( 0 );
 
 
-    this._nextButton.addEventListener('click', this._onNextClick(i));
+    this._nextButton.addEventListener( 'click', this._onNextClick );
 
-    this._closeButton.addEventListener('click', this._onCloseClick);
-  
+    this._closeButton.addEventListener( 'click', this._onCloseClick );
+
   };
 
-  Gallery.prototype.hide = function() {
-    this.element.classList.add('hidden');
-    this._closeButton.removeEventListener('click', this._onCloseClick);
+  Gallery.prototype.hide = function () {
+    this.element.classList.add( 'hidden' );
+    this._closeButton.removeEventListener( 'click', this._onCloseClick );
   };
 
-  Gallery.prototype._onCloseClick = function() {
+  Gallery.prototype._onCloseClick = function () {
     this.hide();
   };
 
-  Gallery.prototype._onNextClick = function (i) {
-    this.setCurrentImage(i)
+  Gallery.prototype._onNextClick = function () {
+    var count = this._currentImage;
+    count++;
+    this.setCurrentImage( count );
   }
 
-  Gallery.prototype.setCurrentImage = function (i) {
-    if (this._currentImage === i) {
+
+
+  Gallery.prototype.setCurrentImage = function ( i ) {
+    if ( this._currentImage === i ) {
       return;
     }
 
-    this._currentImage = i;
+    if ( this.data.pictures[ i ] ) {
+      this._currentImage = i;
 
-    if (this.element.querySelector('img.selected')) {
-      this.element.querySelector('img.selected').classList.remove('selected');
+      if ( this.element.querySelector( 'img.selected' ) ) {
+        this.element.querySelector( 'img.selected' ).classList.remove( 'selected' );
+      }
+      this.element.querySelectorAll( '.gallery-overlay__thumbnails img' )[ i ].classList.add( 'selected' );
+
+      var image = new Image();
+      image.src = this.data.pictures[ i ];
+
+      var previewContainer = this.element.querySelector( '.gallery-overlay__preview' );
+
+      while ( previewContainer.firstChild ) {
+        console.log( previewContainer.firstChild )
+        previewContainer.removeChild( previewContainer.firstChild );
+      }
+
+      previewContainer.appendChild( image );
     }
-    this.element.querySelectorAll('.gallery-overlay__thumbnails img')[i].classList.add('selected');
 
-    var image = new Image();
-    image.src = this.data.pictures[i];
 
-    var previewContainer = this.element.querySelector('.gallery-overlay__preview');
-
-    while(previewContainer.firstChild) {
-      console.log(previewContainer.firstChild)
-      previewContainer.removeChild(previewContainer.firstChild);
-    }
-
-    previewContainer.appendChild(image);
   };
 
   window.Gallery = Gallery;
-})();
+} )();
